335398f5e089e7b174aba5de2fc44d91d34f376a
Matheus Santos Carvalho <matheus.carvalho@merx.tech>
2024-01-21 19:39:30 -0300

    Subida da funcionalidade

---
+++ script.js
@@ -0,0 +1,13 @@
+var modal = document.querySelector('.fade');
+var whatsapp = document.querySelector('.whatsapp');
+var instagram = document.querySelector('.insta');
+var twitter = document.querySelector('.twitter');
+
+
+function abreModal(){
+    modal.style.display = 'flex';
+}
+
+function fechaModal(){
+    modal.style.display = 'none';
+}
