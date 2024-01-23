f24a7136c5b47451b613e55142703c94b3870268
Matheus Santos Carvalho <matheus.carvalho@merx.tech>
2024-01-23 17:54:00 -0300

    ajuste rolagem

--- script.js
+++ script.js
@@ -11,3 +11,17 @@ function abreModal(){
 function fechaModal(){
     modal.style.display = 'none';
 }
+
+document.querySelectorAll('.nav-link').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        window.scrollTo({
+            top: targetElement.offsetTop - document.querySelector('.header').offsetHeight,
+            behavior: 'smooth'
+        });
+    });
+});
